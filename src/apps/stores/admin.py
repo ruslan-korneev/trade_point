2fcd83a80e48efccd3e994ea45a28a18608911c8
Ruslan <kornerus@fjfi.cvut.cz>
2022-02-08 20:01:31 +0300

    debug search fields

--- src/apps/stores/admin.py
+++ src/apps/stores/admin.py
@@ -11,4 +11,4 @@ class StoreAdmin(admin.ModelAdmin):
 @admin.register(Visit)
 class VisitAdmin(admin.ModelAdmin):
     list_display = ('store_and_employee', 'visit_date', 'longitude', 'latitude')
-    search_fields = ('employee__name', 'store__title')
+    search_fields = ('store__employee__name', 'store__title')
